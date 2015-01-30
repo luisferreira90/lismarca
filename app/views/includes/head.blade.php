aef5283a77ee5cb6df6c2147b25d6cc83dd6b2fc
Luís Ferreira <miguelf90@gmail.com>
2015-01-30 19:57:16 +0000

    Criado slideshow jQuery na home.

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -7,8 +7,49 @@
 <link rel="stylesheet" href="css/normalize.css">
 <link rel="stylesheet" href="css/style.css">
 
+<link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+
 <script src = "js/html5shiv-master/dist/html5shiv.js"></script>
 <script src = "js/html5shiv-master/dist/html5shiv-printshiv.js"></script>
 
 <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
+<script src="js/bjqs.min.js"></script>
+<link type="text/css" rel="Stylesheet" href="css/bjqs.css" />
+
+<script>
+
+jQuery(document).ready(function($) {
+	$('#slideshow').bjqs({
+		// width and height need to be provided to enforce consistency
+		// if responsive is set to true, these values act as maximum dimensions
+		'width' : 1000,
+		'height' : 400,
+
+		// animation values
+		'animtype' : 'fade', // accepts 'fade' or 'slide'
+		'animduration' : 450, // how fast the animation are
+		'animspeed' : 3500, // the delay between each slide
+		'automatic' : false, // automatic
+
+		// control and marker configuration
+		'showcontrols' : false, // show next and prev controls
+		'centercontrols' : true, // center controls verically
+		'nexttext' : 'Next', // Text for 'next' button (can use HTML)
+		'prevtext' : 'Prev', // Text for 'previous' button (can use HTML)
+		'showmarkers' : true, // Show individual slide markers
+		'centermarkers' : false, // Center markers horizontally
+
+		// interaction values
+		'keyboardnav' : true, // enable keyboard navigation
+		'hoverpause' : true, // pause the slider on hover
+
+		// presentational options
+		'usecaptions' : false, // show captions for images using the image title tag
+		'randomstart' : true, // start slider at random slide
+		'responsive' : true // enable responsive capabilities (beta)
+	});
+});
+
+</script>
--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
-<p>CONTENT</p>
+<div id="slideshow">
+	<ul class="bjqs">
+		<li><img src="images/slideshow/1.jpg"></li>
+		<li><img src="images/slideshow/2.jpg"></li>
+		<li><img src="images/slideshow/3.jpg"></li>
+	</ul>
+</div>
+
+
 
 @stop
