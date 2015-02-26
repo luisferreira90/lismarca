9ab3af42180a98a6828aa5e7d7b0dde80472e150
Luís Ferreira <miguelf90@gmail.com>
2015-02-26 12:26:25 +0000

    Iniciado trabalho no footer

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,3 +1,26 @@
 <footer>
-	Copyright © 2015 Lismarca - Sistemas de Sombreamento | Mobiliário para Hotelaria. Todo os direitos reservados.
+	<div class = 'footer-upper'>
+		<div class = 'newsletter'>
+			<h2 class = 'title-smaller red '>.NEWSLETTER</h2>
+			<form>
+				<input type = 'email' placeholder = 'E-mail' name = 'email'>
+			</form>
+			<img src = 'images/email-big.png'>
+		</div>
+		<nav>
+			<ul>
+				<li>.Empresa</li>
+				<li>.Produtos</li>
+				<li>.Contactos</li>
+				<li>.Novidades</li>
+				<li>.Portfolio</li>
+			</ul>
+		</nav>
+	</div>
+	<div class = 'clear-fix'></div>
+	<div class = 'footer-middle'>
+	</div>
+	<div class = 'footer-lower'>
+		Copyright © 2015 Lismarca - Sistemas de Sombreamento | Mobiliário para Hotelaria. Todo os direitos reservados.
+	</div>
 </footer>
