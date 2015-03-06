3e381ac4faa0227451868fe2ae9059cbb013e66f
Luís Ferreira <miguelf90@gmail.com>
2015-03-06 11:54:47 +0000

    Removida route desnecessária

--- app/routes.php
+++ app/routes.php
@@ -16,17 +16,15 @@ Route::get('/', function()
 	return View::make('pages/home');
 });
 
-Route::resource('registo', 'UsersController'); 
-
 Route::get('registo', array('as' => 'registo', 'uses' => 'UsersController@registration'));
 
 Route::get('login', array('as' => 'login', 'uses' => 'UsersController@login'));
 
 Route::post('/login', array('as' => 'login', 'uses' => 'UsersController@handleLogin'));
 
-Route::get('/profile', array('as' => 'profile', 'uses' => 'UsersController@profile'));
-
 Route::get('/logout', array('as' => 'logout', 'uses' => 'UsersController@logout'));
+
+Route::get('/profile', array('as' => 'profile', 'uses' => 'UsersController@profile'));
 
 Route::resource('user', 'UsersController'); 
 
