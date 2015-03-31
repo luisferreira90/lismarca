b9e6935fbf07e4f6b2c400c9b0fd85c4d699d9cf
Luís Ferreira <miguelf90@gmail.com>
2015-03-31 19:50:45 +0100

    Removido lixo do header e do footer (mas ainda por arranjar) e adicionada bandeira da Grã-Bretanha

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
 	<div class = 'footer-upper'>
 		<div class = 'newsletter'>
-			<h2 class = 'title-smaller red '>.NEWSLETTER</h2>
+			<h2 class = 'title-smaller'>.NEWSLETTER</h2>
 			<form>
 				<input type = 'email' placeholder = 'E-mail' name = 'email'>
 			</form>
@@ -19,56 +19,6 @@
 	</div>
 	<div class = 'clear-fix'></div>
 
-	<div class = 'three-columns'>
-		<div class = 'column'>
-			<div class = 'footer-about-left'>
-				<h3>.SOBRE NÓS</h3>
-				<ul>
-					<li>Entrar em contacto</li>
-					<li>Termos de Serviço</li>
-					<li>Politica de Privacidade</li>
-				</ul>
-			</div>
-			<div class = 'footer-about-right'>
-				<h3>.SOS</h3>
-				<ul>
-					<li>Perguntas Frequentes</li>
-					<li>Guia de Conteúdo</li>
-					<li>Desenvolvimento</li>
-				</ul>
-			</div>
-		</div>
-		<div class = 'column middle'>
-			<div class = 'distribution'>
-				<img class = 'arrow-right' src = '/images/arrow-right.svg'>
-				<p>Fornecemos para todo o país: Portugal Continental e Ilhas</p>
-			</div>
-			<div class = 'separator'></div>
-			<div class = 'social-networks'>
-				<img src = '/images/social-facebook.svg'>
-				<img src = '/images/social-youtube.svg'>
-				<img src = '/images/social-linkedin.svg'>
-				<img src = '/images/social-gplus.svg'>
-				<img src = '/images/social-dropbox.svg'>
-				<img src = '/images/social-evernote.svg'>
-				<img src = '/images/social-pinterest.svg'>
-			</div>
-		</div>
-		<div class = 'column right'>
-			<div class = 'phone'>
-				<img src = '/images/phone.svg'>
-				<p>291 741 884 | </p>
-				<img src = '/images/mobile-phone.svg'>
-				<p>965 010 699</p>
-			</div>
-			<div class = 'email'>
-				<img src = '/images/email.svg'><a href = 'mailto:info@lismarca.pt'>info@lismarca.pt</a> | <a href = 'mailto:web@lismarca.pt'>web@lismarca.pt</a>
-			</div>
-			<div class = 'address'>
-				<img src = '/images/location.svg'><p>Rua Mestre Sidónio 9A, 9020-365, Funchal<br>Madeira | Portugal</p>
-			</div>
-		</div>
-	</div>
 	<div class = 'clear-fix'></div>
 	<div class = 'separator'></div>
 	<div class = 'footer-lower'>
--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -1,42 +1,10 @@
 <header>
-	<div class = 'upper-header'>
-		<div class = 'upper-header-content'>
-			<div class = 'upper-header-left'>
+	<div class = 'lower-header'>
+		<div class = 'lower-header-left'>
+			<a href = '/'><img src = '/images/logo.svg' alt = 'Logo Lismarca' class = 'logo'></a>
+		</div>
 
-				<div class = 'language'>
-					@if(Session::get('language') === 'en') 
-						<img class = 'language-flag' src = '/images/flag-icon-pt.svg'>
-						<p class = 'language-name'><a href = 'language?lang=pt'>Portuguese</a></p>
-					@else
-						<img class = 'language-flag' src = '/images/flag-icon-en.svg'>
-						<p class = 'language-name'><a href = 'language?lang=en'>English</a></p>
-					@endif		
-					<img src = '/images/arrow-down.svg'>
-				</div>
-
-				<div class = 'phone'>
-					<img src = '/images/phone.svg'>
-					<p>291 741 884</p>
-				</div>
-
-				<div class = 'mobile-phone'>
-					<img src = '/images/mobile-phone.svg'>
-					<p>965 010 699</p>
-				</div>
-
-				<div class = 'facebook'>
-					<img src = '/images/fb-icon.svg'>
-					<p class = 'blue'>gosto</p>
-				</div>
-
-				<div class = 'distribution'>
-					<img class = 'arrow-right' src = '/images/arrow-right.svg'>
-					<p>Fornecemos para todo o país: Portugal Continental e Ilhas</p>
-				</div>
-
-			</div>
-
-			<div class = 'login'>
+		<div class = 'login'>
 				<div class = 'login-register'>
 					<img class = 'arrow-right' src = '/images/arrow-right.svg'>
 					@if(Auth::check() && User::isAdmin())
@@ -57,19 +25,25 @@
 					@endif
 				</div>
 			</div>
-		</div> 
-	</div>
 
-	<div class = 'lower-header'>
-		<div class = 'lower-header-left'>
-			<a href = '/'><img src = '/images/logo.svg' alt = 'Logo Lismarca' class = 'logo'></a>
+		<div class = 'language'>
+			@if(Session::get('language') === 'en') 
+				<img class = 'language-flag' src = '/images/flag-icon-pt.svg'>
+				<p class = 'language-name'><a href = 'language?lang=pt'>Portuguese</a></p>
+			@else
+				<img class = 'language-flag' src = '/images/flag-icon-en.svg'>
+				<p class = 'language-name'><a href = 'language?lang=en'>English</a></p>
+			@endif		
+			<img src = '/images/arrow-down.svg'>
 		</div>
 
 		<div class = 'lower-header-right'>
-			<div class = 'validate-budget'>
-				<img src = '/images/arrow-right-red.svg'>
-				<p>.VALIDAR ORÇAMENTO</p>
-			</div>
+			@if(Auth::check())
+				<div class = 'validate-budget'>
+					<img src = '/images/arrow-right-red.svg'>
+					<p>.VALIDAR ORÇAMENTO</p>
+				</div>
+			@endif
 
 			<div class = 'search'>
 				<form class='search_field' method='GET' action='http://www.google.com/search'>
@@ -79,6 +53,9 @@
 				    </button>
 				</form>
 			</div>	
+
+
+			
 
 			<nav>
 				<ul>
