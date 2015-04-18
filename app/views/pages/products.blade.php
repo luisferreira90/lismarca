4c49962e3453e98e482c7ce61661f59aa6404cde
Luís Ferreira <miguelf90@gmail.com>
2015-04-18 18:57:31 +0100

    Iniciado trabalho na página dos produtos

--- app/views/pages/products.blade.php
+++ app/views/pages/products.blade.php
@@ -4,6 +4,71 @@
 
 <h1>Produtos</h1>
 
-Página dos produtos - Em construção
+<section class = 'products'>
+	<div class = 'three-columns'>
+		<div>
+			<a href = '#1'>
+				<img class="bottom" src="/images/home/1t.jpg" />
+				<img class="top" src="/images/home/1.jpg" />
+			</a>
+		</div>
+		<div>
+			<a href = '#2'>
+				<img class="bottom" src="/images/home/2t.jpg" />
+				<img class="top" src="/images/home/2.jpg" />
+			</a>
+		</div>
+		<div>
+			<a href = '#3'>
+				<img class="bottom" src="/images/home/3t.jpg" />
+				<img class="top" src="/images/home/3.jpg" />
+			</a>
+		</div>
+	</div>
+
+	<div class = 'three-columns'>
+		<div>
+			<a href = '#4'>
+				<img class="bottom" src="/images/home/4t.jpg" />
+				<img class="top" src="/images/home/4.jpg" />
+			</a>
+		</div>
+		<div>
+			<a href = '#5'>
+				<img class="bottom" src="/images/home/5t.jpg" />
+				<img class="top" src="/images/home/5.jpg" />
+			</a>
+		</div>
+		<div>
+			<a href = '#6'>
+				<img class="bottom" src="/images/home/6t.jpg" />
+				<img class="top" src="/images/home/6.jpg" />
+			</a>
+		</div>
+	</div>
+
+	<div class = 'three-columns'>
+		<div>
+			<a href = '#7'>
+				<img class="bottom" src="/images/home/7t.jpg" />
+				<img class="top" src="/images/home/7.jpg" />
+			</a>
+		</div>
+		<div>
+			<a href = '#8'>
+				<img class="bottom" src="/images/home/8t.jpg" />
+				<img class="top" src="/images/home/8.jpg" />
+			</a>
+		</div>
+		<div>
+			<a href = '#9'>
+				<img class="bottom" src="/images/home/9t.jpg" />
+				<img class="top" src="/images/home/9.jpg" />
+			</a>
+		</div>
+	</div>
+</section>
+
+{{ HTML::style('css/products.css') }}
 
 @stop
