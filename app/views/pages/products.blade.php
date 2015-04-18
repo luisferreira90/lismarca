b6b7dd3e9b542d2117ea8d7764d83875633ca229
Luís Ferreira <miguelf90@gmail.com>
2015-04-18 20:00:28 +0100

    Feito código para galeria de itens e categorias/subcategorias/etc. Falta aprovação do cliente e algumas melhorias

--- app/views/pages/products.blade.php
+++ app/views/pages/products.blade.php
@@ -4,70 +4,52 @@
 
 <h1>Produtos</h1>
 
-<section class = 'products'>
-	<div class = 'three-columns'>
-		<div>
-			<a href = '#1'>
-				<img class="bottom" src="/images/home/1t.jpg" />
-				<img class="top" src="/images/home/1.jpg" />
-			</a>
-		</div>
-		<div>
-			<a href = '#2'>
-				<img class="bottom" src="/images/home/2t.jpg" />
-				<img class="top" src="/images/home/2.jpg" />
-			</a>
-		</div>
-		<div>
-			<a href = '#3'>
-				<img class="bottom" src="/images/home/3t.jpg" />
-				<img class="top" src="/images/home/3.jpg" />
-			</a>
-		</div>
-	</div>
+<div class="products">
+    <div class="galleryItem">
+        <a href="#1"><img src="/images/home/1.jpg" class = 'top' alt=""></a>
+        <h3>Title</h3>       
+    </div>
+ 
+    <div class="galleryItem">
+        <a href="#1"><img src="/images/home/2.jpg" alt=""></a>
+        <h3>Title</h3>        
+    </div>
 
-	<div class = 'three-columns'>
-		<div>
-			<a href = '#4'>
-				<img class="bottom" src="/images/home/4t.jpg" />
-				<img class="top" src="/images/home/4.jpg" />
-			</a>
-		</div>
-		<div>
-			<a href = '#5'>
-				<img class="bottom" src="/images/home/5t.jpg" />
-				<img class="top" src="/images/home/5.jpg" />
-			</a>
-		</div>
-		<div>
-			<a href = '#6'>
-				<img class="bottom" src="/images/home/6t.jpg" />
-				<img class="top" src="/images/home/6.jpg" />
-			</a>
-		</div>
-	</div>
+    <div class="galleryItem">
+        <a href="#1"><img src="/images/home/3.jpg" alt=""></a>
+        <h3>Title</h3>       
+    </div>
 
-	<div class = 'three-columns'>
-		<div>
-			<a href = '#7'>
-				<img class="bottom" src="/images/home/7t.jpg" />
-				<img class="top" src="/images/home/7.jpg" />
-			</a>
-		</div>
-		<div>
-			<a href = '#8'>
-				<img class="bottom" src="/images/home/8t.jpg" />
-				<img class="top" src="/images/home/8.jpg" />
-			</a>
-		</div>
-		<div>
-			<a href = '#9'>
-				<img class="bottom" src="/images/home/9t.jpg" />
-				<img class="top" src="/images/home/9.jpg" />
-			</a>
-		</div>
-	</div>
-</section>
+    <div class="galleryItem">
+        <a href="#1"><img src="/images/home/4.jpg" alt=""></a>
+        <h3>Title</h3>        
+    </div>
+
+    <div class="galleryItem">
+        <a href="#1"><img src="/images/home/5.jpg" alt=""></a>
+        <h3>Title</h3>        
+    </div>
+
+    <div class="galleryItem">
+        <a href="#1"><img src="/images/home/6.jpg" alt=""></a>
+        <h3>Title</h3>        
+    </div>
+
+    <div class="galleryItem">
+        <a href="#1"><img src="/images/home/7.jpg" alt=""></a>
+        <h3>Title</h3>        
+    </div>
+
+    <div class="galleryItem">
+        <a href="#1"><img src="/images/home/8.jpg" alt=""></a>
+        <h3>Title</h3>        
+    </div>
+
+    <div class="galleryItem">
+        <a href="#1"><img src="/images/home/9.jpg" alt=""></a>
+        <h3>Title</h3>        
+    </div>
+</div>
 
 {{ HTML::style('css/products.css') }}
 
