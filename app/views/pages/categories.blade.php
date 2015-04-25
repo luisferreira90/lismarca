37ae8b2544af722701d80c80d443384f599c8e60
Luís Ferreira <miguelf90@gmail.com>
2015-04-25 09:47:10 +0100

    Adicionada DIV às imagens dos produtos, com cor de fundo vermelha. Agora ao fazer hover nas imagens, estas ficam com opacidade e com efeito de filtro vermelho, e também foi aplicado um CSS transform para a imagem sobressair.

--- app/views/pages/categories.blade.php
+++ app/views/pages/categories.blade.php
@@ -6,47 +6,47 @@
 
 <div class="categories">
     <div class="galleryItem">
-        <a href="produtos/categoria"><img src="/images/home/1.jpg" alt=""></a>
+        <div class='image'><a href="produtos/categoria"><img src="/images/home/1.jpg" alt=""></a></div>
         <h3>.Title</h3>       
     </div>
  
     <div class="galleryItem">
-        <a href="produtos/categoria"><img src="/images/home/2.jpg" alt=""></a>
+        <div class='image'><a href="produtos/categoria"><img src="/images/home/2.jpg" alt=""></a></div>
         <h3>.Title</h3>        
     </div>
 
     <div class="galleryItem">
-        <a href="produtos/categoria"><img src="/images/home/3.jpg" alt=""></a>
+        <div class='image'><a href="produtos/categoria"><img src="/images/home/3.jpg" alt=""></a></div>
         <h3>.Title</h3>       
     </div>
 
     <div class="galleryItem">
-        <a href="produtos/categoria"><img src="/images/home/4.jpg" alt=""></a>
+        <div class='image'><a href="produtos/categoria"><img src="/images/home/4.jpg" alt=""></a></div>
         <h3>.Title</h3>        
     </div>
 
     <div class="galleryItem">
-        <a href="produtos/categoria"><img src="/images/home/5.jpg" alt=""></a>
+        <div class='image'><a href="produtos/categoria"><img src="/images/home/5.jpg" alt=""></a></div>
         <h3>.Title</h3>        
     </div>
 
     <div class="galleryItem">
-        <a href="produtos/categoria"><img src="/images/home/6.jpg" alt=""></a>
+        <div class='image'><a href="produtos/categoria"><img src="/images/home/6.jpg" alt=""></a></div>
         <h3>.Title</h3>        
     </div>
 
     <div class="galleryItem">
-        <a href="produtos/categoria"><img src="/images/home/7.jpg" alt=""></a>
+        <div class='image'><a href="produtos/categoria"><img src="/images/home/7.jpg" alt=""></a></div>
         <h3>.Title</h3>        
     </div>
 
     <div class="galleryItem">
-        <a href="produtos/categoria"><img src="/images/home/8.jpg" alt=""></a>
+        <div class='image'><a href="produtos/categoria"><img src="/images/home/8.jpg" alt=""></a></div>
         <h3>.Title</h3>        
     </div>
 
     <div class="galleryItem">
-        <a href="produtos/categoria"><img src="/images/home/9.jpg" alt=""></a>
+        <div class='image'><a href="produtos/categoria"><img src="/images/home/9.jpg" alt=""></a></div>
         <h3>.Title</h3>        
     </div>
 </div>
