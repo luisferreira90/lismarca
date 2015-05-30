fd33aec5cbadf590713f68de6f555e54aeb30af7
Luís Ferreira <miguelf90@gmail.com>
2015-05-30 09:53:52 +0100

    Filtros a funcionar para items no backoffice

--- app/controllers/admin/ItemsController.php
+++ app/controllers/admin/ItemsController.php
@@ -35,8 +35,28 @@ class ItemsController extends \BaseController {
 	public function items() {
 		$item = new ProductItem;
 		$items = $item->listAll(Input::all());
-  		return View::make('admin.item')->with('items', $items)
-  		/*->with('subcategories', ProductSubcategory::lists('name', 'id'))*/;
+  		$view = View::make('admin.item')->with('items', $items)
+  		->with('sections', ProductSection::lists('name', 'id'));
+
+		if(Input::has('section') && Input::has('subsection') && Input::has('category')) {
+			$subcategory = new ProductSubcategory;
+			$subcategories = $subcategory->where('category', '=', Input::get('category'))->lists('name', 'id');
+			$view = $view->with('subcategories', $subcategories);	
+		}
+
+		if(Input::has('section') && Input::has('subsection')) {
+			$category = new ProductCategory;
+			$categories = $category->where('subsection', '=', Input::get('subsection'))->lists('name', 'id');
+			$view = $view->with('categories', $categories);
+		}
+
+		if(Input::has('section')) {
+			$subsection = new ProductSubsection;
+			$subsections = $subsection->where('section', '=', Input::get('section'))->lists('name', 'id');
+			$view = $view->with('subsections', $subsections);
+		}
+
+  		return $view;
 	}
 
 
--- app/views/admin/item.blade.php
+++ app/views/admin/item.blade.php
@@ -4,16 +4,38 @@
 
 <h1>Gestão de items</h1>
 
-{{--
 <div class = 'filters'>
-	{{ Form::open(array('url' => 'admin/items', 'method' => 'get', 'class' => 'form-inline')) }}
-		<div class = 'form-group'>
-			{{Form::label('subcategory','Subcategoria')}}	
-			{{Form::select('subcategory', [null=>'Sem filtro']+$subcategories, Input::get('subcategory'), array('class' => 'form-control', 'onchange' => 'this.form.submit()'))}}
-		</div>
-    {{ Form::close() }}
+    {{ Form::open(array('url' => 'admin/items', 'method' => 'GET', 'class' => 'form-inline')) }}
+        <div class = 'form-group'>
+            {{Form::label('section','Secção')}}   
+            {{Form::select('section', [null=>'Sem filtro']+$sections, Input::get('section'), array('class' => 'form-control', 'onchange' => 'this.form.submit()'))}}
+        </div>
+
+        @if(isset($subsections))
+            <div class = 'form-group'>
+                {{Form::label('subsection','Sub-Secção')}}   
+                {{Form::select('subsection', [null=>'Sem filtro']+$subsections, Input::get('subsection'), array('class' => 'form-control', 'onchange' => 'this.form.submit()'))}}
+            </div>
+        @endif
+
+        @if(isset($categories))
+            <div class = 'form-group'>
+                {{Form::label('category','Categoria')}}   
+                {{Form::select('category', [null=>'Sem filtro']+$categories, Input::get('category'), array('class' => 'form-control', 'onchange' => 'this.form.submit()'))}}
+            </div>
+        @endif
+
+        @if(isset($subcategories))
+            <div class = 'form-group'>
+                {{Form::label('subcategory','Sub-Categoria')}}   
+                {{Form::select('subcategory', [null=>'Sem filtro']+$subcategories, Input::get('subcategory'), array('class' => 'form-control', 'onchange' => 'this.form.submit()'))}}
+            </div>
+        @endif
+
+    {{ Form::close() }}    
+    
 </div>
---}}
+
 <div class = 'novo'>
 	<a href = 'items/criar'><button class="btn btn-primary">Novo</button></a>
 </div>
@@ -24,7 +46,6 @@
 			<tr>
 				<th><span>ID</span></th>
 				<th><span>Nome</span></th>
-				<th><span>Sub-Categoria</span></th>
 				<th><span>Publicar</span></th>
 				<th><span>Apagar</span></th>
 			</tr>
@@ -34,7 +55,6 @@
 				<tr>
 					<td><a href = 'items/{{ $item->id }}'>{{ $item->id }}</a></td>
 					<td>{{ $item->name }}</td>
-					{{--<td>{{ $subcategories[$item->subcategory] }}</td>--}}
 					<td>
 						@if($item->published == 1)
 							<a href = 'items/unpublish/{{ $item->id }}'><button class="btn btn-warning btn-sm">Despublicar</button></a>
