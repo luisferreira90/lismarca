f71764661014e73c36b36106ff3d8d4b469c7c83
Luís Ferreira <miguelf90@gmail.com>
2015-06-24 14:15:08 +0100

    Carrinho correcto - adiciona e actualiza

--- app/controllers/CartsController.php
+++ app/controllers/CartsController.php
@@ -3,8 +3,14 @@
 class CartsController extends BaseController {
 
 	public function store() {
-		$data = Input::all();
-		$new = Cart::create($data);
+
+		DB::statement('
+			INSERT INTO carts (product_item, quantity, user) 
+			VALUES (' .
+			Input::get('product_item') . ','.
+			Input::get('quantity') . ',' .
+			Input::get('user') . ') ON DUPLICATE KEY UPDATE quantity = ' . Input::get('quantity')
+			);
 		return Redirect::back()->withInput();
 	}
 
