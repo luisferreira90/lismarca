4478a5c03a94f6b8ede4ab8bcf07604b14871bd3
Luís Ferreira <miguelf90@gmail.com>
2015-06-24 17:21:44 +0100

    Divs adicionads para melhor debugging

--- app/views/pages/checkout.blade.php
+++ app/views/pages/checkout.blade.php
@@ -5,9 +5,13 @@
 <div class = 'carts'>
 
 	@foreach($cart as $item)
+	<div class = ''>
+		Id:{{$item->product_item}} <br>
 		Nome:{{$item->name}} <br>
 		Quantidade:{{$item->quantity}} <br>
 		Icon:{{$item->icon}} <br>
+	</div>
+	<br><br>
 	@endforeach
 
 </div>
