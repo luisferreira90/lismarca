8a1deb80bed1caac01172298fdcc670718ef955a
Luís Ferreira <miguelf90@gmail.com>
2015-06-27 19:19:57 +0100

    Página de carrinho em tabela

--- app/views/pages/checkout.blade.php
+++ app/views/pages/checkout.blade.php
@@ -1,18 +1,42 @@
+{{ HTML::style('css/slick.css') }}
+{{ HTML::style('css/slick-theme.css') }}
+{{ HTML::style('css/bootstrap.css') }}
+{{ HTML::style('css/products.css') }}
+
 @extends('layouts.layout')
 
 @section('content')
 
 <div class = 'carts'>
 
-	@foreach($cart as $item)
-	<div class = ''>
-		Id:{{$item->product_item}} <br>
-		Nome:{{$item->name}} <br>
-		Quantidade:{{$item->quantity}} <br>
-		Icon:{{$item->icon}} <br>
-	</div>
-	<br><br>
-	@endforeach
+	<table class = 'table table-striped'>
+		<thead>
+			<tr>
+				<th></th>
+				<th>Nome</th>
+				<th>Quantidade</th>
+				<th>Eliminar</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($cart as $product)
+				<tr>
+					<td>
+						<a href={{'/produtos/' . $product->id }}>{{ HTML::image('images/produtos/icons/' . $product->icon) }}</a>
+					</td>
+					<td>
+						{{$product->name}}
+					</td>
+					<td>
+						{{$product->quantity}}
+					</td>
+					<td>
+						Eliminar
+					</td>
+				</tr>
+			@endforeach
+	</tbody>
+	</table>
 
 </div>
 
