dd94f2a65252e5fa431cac34fdaacf0e308a1cda
Luís Ferreira <miguelf90@gmail.com>
2015-08-05 09:22:03 +0100

    Dados já aparecem correctamente nas encomendas (faltam os produtos encomendados

--- app/views/admin/order-edit.blade.php
+++ app/views/admin/order-edit.blade.php
@@ -11,41 +11,40 @@
       {{$message}}
   @endforeach
 
-  {{ Form::model($order, array('route' => array('admin.orders.update', $order->id), 'method' => 'PUT', 'files' => true, 'class' => 'form-horizontal')) }}
+  {{ Form::model($order[0], array('route' => array('admin.order.update', $order[0]->id), 'method' => 'PUT', 'files' => true, 'class' => 'form-horizontal')) }}
 
   <div class = 'form-group'>
       {{Form::label('name', 'Nome')}}
-      {{Form::text('name', null, array('class' => 'form-control'))}}
+      {{Form::text('name', null, array('class' => 'form-control', 'disabled'))}}
   </div>    
 
   <div class = 'form-group'>
       {{Form::label('company_name', 'Empresa')}}
-      {{Form::text('company_name', null, array('class' => 'form-control'))}}
+      {{Form::text('company_name', null, array('class' => 'form-control', 'disabled'))}}
   </div>
 
   <div class = 'form-group'>
       {{Form::label('email', 'Email')}}
-      {{Form::text('email', null, array('class' => 'form-control'))}}
+      {{Form::text('email', null, array('class' => 'form-control', 'disabled'))}}
   </div>
 
   <div class = 'form-group'>
       {{Form::label('phone', 'Telefone')}}
-      {{Form::text('phone', null, array('class' => 'form-control'))}}
+      {{Form::text('phone', null, array('class' => 'form-control', 'disabled'))}}
   </div>
 
   <div class = 'form-group'>
       {{Form::label('address', 'Morada')}}
-      {{Form::text('address', null, array('class' => 'form-control'))}}
+      {{Form::text('address', null, array('class' => 'form-control', 'disabled'))}}
   </div>
 
   <div class = 'form-group'>
       {{Form::label('datetime', 'Data')}}
-      {{Form::text('datetime', null, array('class' => 'form-control'))}}
+      {{Form::text('datetime', null, array('class' => 'form-control', 'disabled'))}}
   </div>
 
   <div class = 'form-group'>
-      {{Form::label('treated', 'Tratada')}}
-      {{Form::text('treated', null, array('class' => 'form-control'))}}
+      {{Form::submit('Marcar como tratada',array('id' => 'submit', 'class' => 'btn btn-success'))}}
   </div>
 
   {{ Form::close() }}
